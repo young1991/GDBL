3f25c46baccce21c5415e0ff08d8ef4ee2c1354f
yang zhang <zhangyanggfkd@gmail.com>
2016-07-27 11:20:29 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -15,7 +15,7 @@ function createEvent (name, maxNbEvents) {
         data: []
     };
     // add up to 200 events
-    d3.csv("./developer_8153.csv",function(error,csv){
+    d3.csv("developer_8153.csv",function(error,csv){
     	if (error) throw error;
 	csv.forEach(function(x){
 	    var time = x.created_at
