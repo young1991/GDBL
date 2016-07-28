f66b0b4262e74da7b4f4c63d45caf4fc54458657
young <yangzai09@foxmail.com>
2016-07-28 12:02:44 +0800

    update all data

--- demo.js
+++ demo.js
@@ -1,52 +1,62 @@
-/*eslint-disable */
+/*generate-behavior-map*/
 
-// create dataset
-var data = [];
-var names = ["Watch", "Fork", "Subscribed", "Comment", "Issue", "Pull_request"];
-var endTime = Date.now();
-var month = 30 * 24 * 60 * 60 * 1000;
-var startTime = endTime - 6 * month;
+function getProjectDeveloper()
+{
+    var project = document.getElementsByName("project");
+    var developer = document.getElementsByName("developer");
+    d3.csv("./behavior_data/project_developer.csv",function(error,csv){
+        if (error) throw error;
+        csv.forEach(function(x){
+           if ((project[0].value==x.project)&(developer[0].value==x.developer))
+           {
+                //alert(x.id);
+                createMap(x.id);
+           }
+        });
+    });
+}
 
 
-function createEvent (name, maxNbEvents) {
-    maxNbEvents = maxNbEvents | 200;
+function createEvent (name,fileID) {
     var event = {
         name: name,
         data: []
     };
-    // add up to 200 events
-    d3.csv("developer_129.csv",function(error,csv){
-    	if (error) throw error;
-	    csv.forEach(function(x){
-	       var time = x.created_at
-	       event.data.push(new Date(time));
-	    });
-    });
-    //console.log(event);
-    //var max =  Math.floor(Math.random() * maxNbEvents);
-    //for (var j = 0; j < max; j++) {
-    //    var time = (Math.random() * (endTime - startTime)) + startTime;
-    //    event.data.push(new Date(time));
-    //}
 
+    d3.csv("./behavior_data/"+fileID+".csv",function(error,csv){
+        if (error) throw error;
+        csv.forEach(function(x){
+            var time = x.time;
+            if (name==x.action) event.data.push(new Date(time));
+        });
+    });
     return event;
 }
-for (var i = 0; i < 6; i++) {
-    data.push(createEvent(names[i]));
+
+function createMap (fileID) {
+    var data = [];
+    var names = ["watch", "fork", "subscribed", "comment", "issue", "pull_request"];
+    var endTime = "2018-12-01 00:00:00"//Date.now();
+    var month = 30 * 24 * 60 * 60 * 1000;
+    var startTime = "2008-01-01 00:00:00"//endTime - 108 * month;
+
+    for (var i = 0; i < 6; i++) {
+        data.push(createEvent(names[i],fileID));
+    }
+
+    var color = d3.scale.category20();
+
+    // create chart function
+    var eventDropsChart = d3.chart.eventDrops()
+        .eventLineColor(function (datum, index) {
+            return color(index);
+        })
+        .start(new Date(startTime))
+        .end(new Date(endTime));
+
+    // bind data with DOM
+    var element = d3.select('#behavior_map').datum(data);
+
+    // draw the chart
+    eventDropsChart(element);
 }
-
-var color = d3.scale.category20();
-
-// create chart function
-var eventDropsChart = d3.chart.eventDrops()
-    .eventLineColor(function (datum, index) {
-        return color(index);
-    })
-    .start(new Date(startTime))
-    .end(new Date(endTime));
-
-// bind data with DOM
-var element = d3.select('#eventdrops-demo').datum(data);
-
-// draw the chart
-eventDropsChart(element);
